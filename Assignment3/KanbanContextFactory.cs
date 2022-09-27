1360090cb5d372e9ea1722fed8cc689714acc9c5
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-09-27 17:27:18 +0200

    remove seed from factory

--- Assignment3/KanbanContextFactory.cs
+++ Assignment3/KanbanContextFactory.cs
@@ -15,21 +15,6 @@ internal class KanbanContextFactory : IDesignTimeDbContextFactory<KanbanContext>
         optionsBuilder.UseNpgsql(connectionString);
 
         var context = new KanbanContext(optionsBuilder.Options);
-        /* context.Destroy(); */
-        /* Seed(context); */
         return context;
-    }
-
-    private static void Seed(KanbanContext context)
-    {
-        var testTag = new Tag { Name = "test" };
-
-        var testUser = new User { Email = "test@itu.dk", Name = "TestUser" };
-
-        var testTask = new Assignment3.Entities.Task { Title = "Do Stuff", AssignedTo = testUser, Description = "Do Some Stuff!!", State = State.New, Tags = new[] { testTag } };
-
-        context.Tasks.Add(testTask);
-
-        context.SaveChanges();
     }
 }
