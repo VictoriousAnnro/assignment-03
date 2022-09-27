908cf8daaadc7539a91986358e11c6997ca7c7f0
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-09-27 16:33:06 +0200

    add created and stateupdated to task

--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -2,11 +2,14 @@ namespace Assignment3.Entities;
 
 public class Task
 {
-
     public int Id { get; set; }
     public string Title { get; set; } = "Untitled Task";
     public User? AssignedTo { get; set; }
     public string? Description { get; set; }
     public State State { get; set; }
     public ICollection<Tag> Tags { get; set; } = new HashSet<Tag>();
+
+    public DateTime Created { get; set; }
+
+    public DateTime StateUpdated { get; set; }
 }
