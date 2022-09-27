d2db569ca0bd3f0d9ee70a3bdf32e087e8d390e8
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-09-27 16:33:52 +0200

    implement taskrepo create

--- Assignment3.Entities/TaskRepository.cs
+++ Assignment3.Entities/TaskRepository.cs
@@ -2,9 +2,34 @@ namespace Assignment3.Entities;
 
 public class TaskRepository : ITaskRepository
 {
+    private KanbanContext _context;
+
+    public TaskRepository(KanbanContext context)
+    {
+        _context = context;
+    }
+
     public (Response Response, int TaskId) Create(TaskCreateDTO task)
     {
-        throw new NotImplementedException();
+        var assignedUser = task.AssignedToId != null ? _context.Users.Find(task.AssignedToId) : null!;
+        if (assignedUser == null && task.AssignedToId != null) return (Response.BadRequest, -1);
+        var tags = task.Tags.Select(t => new Tag { Name = t }).ToHashSet();
+
+        var newTask = new Task
+        {
+            Title = task.Title,
+            AssignedTo = assignedUser,
+            Description = task.Description,
+            State = State.New,
+            Tags = tags,
+            Created = DateTime.UtcNow,
+            StateUpdated = DateTime.UtcNow
+        };
+
+        _context.Tasks.Add(newTask);
+        _context.SaveChanges();
+
+        return (Response.Created, newTask.Id);
     }
 
     public Response Delete(int taskId)
